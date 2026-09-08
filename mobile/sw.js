e90afcbb94c47eb14508bc951f53a4d46d506ef0
geylsonalbuquerque39-maker <geylsonalbuquerque39@gmail.com>
2026-09-08 16:54:13 -0300

    Add files via upload

--- mobile/sw.js
+++ mobile/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'erp-v10-mobile-m2';
+const CACHE = 'erp-v10-mobile-m4';
 const CORE = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).catch(()=>{})); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())); });
