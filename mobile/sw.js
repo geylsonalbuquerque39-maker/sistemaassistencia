dfad52f669be2f2154f8bd8e30112766ac0a3401
geylsonalbuquerque39-maker <geylsonalbuquerque39@gmail.com>
2026-09-09 11:01:28 -0300

    Add files via upload

--- mobile/sw.js
+++ mobile/sw.js
@@ -1,4 +1,4 @@
-const CACHE='erp-v10-coleta-fotos-cf7';
+const CACHE='erp-v10-coleta-fotos-cf8';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).catch(()=>{}))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
